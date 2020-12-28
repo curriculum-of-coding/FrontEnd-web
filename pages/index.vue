ee1cd6533e22db86b2d80a699ae6688e8fcd756e
DESKTOP-TA0OEQM\whdgn <leejonghun3245@ex-it.co.kr>
2020-12-28 11:09:11 +0900

    test

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+        테스트입니다
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
