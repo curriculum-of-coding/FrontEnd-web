8213dc95c7995119ee7daf258ef2ae5212658fd4
종훈 <leejonghun>
2021-01-19 19:59:56 +0900

    add compotition

--- plugins/plugins.js
+++ plugins/plugins.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import VueLodash from 'vue-lodash'
 import lodash from 'lodash'
+import VueCompositionApi from '@vue/composition-api'
 Vue.use(VueLodash, { name: 'custom' , lodash: lodash })
+Vue.use(VueCompositionApi)
+
 
