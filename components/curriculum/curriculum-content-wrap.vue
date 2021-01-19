dcee709c42c4ba9a0c6c46ba02a6919aa327e43d
종훈 <leejonghun>
2021-01-19 23:24:32 +0900

    add curriculum compontents

---
+++ components/curriculum/curriculum-content-wrap.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="curriculum_content_wrap">
+    <curriculum-title></curriculum-title>
+    <swiper ref="curriculumSwiper" :options="swiperOption" style="width: 100%;">
+      <swiper-slide v-for="data in ddd.length">
+        <curriculum-content></curriculum-content>
+      </swiper-slide>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import {Swiper, SwiperSlide} from 'vue-awesome-swiper'
+import curriculumContent from '~/components/curriculum/curriculum-content'
+import curriculumTitle from '~/components/curriculum/curriculum-title.vue'
+
+export default {
+  name: 'curriculum-content-wrap',
+  components: {
+    curriculumContent,
+    curriculumTitle,
+    Swiper,
+    SwiperSlide
+  },
+  data() {
+    return {
+      ddd: [1, 2, 23, 3, 2],
+      swiperOption: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+
+      }
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.curriculumSwiper.$swiper
+    }
+  },
+  mounted() {
+    console.log('Current Swiper instance object', this.swiper)
+    this.swiper.slideTo(3, 1000, false)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+#curriculum_content_wrap {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  flex-direction: column;
+  .swiper-button-prev:after, .swiper-button-next:after{
+    font-size: 14px;
+  }
+  .swiper-button-prev,.swiper-button-next{
+    background: #484747 0 0 no-repeat padding-box;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    color: #FFFFFF;
+    font-size: 20px;
+  }
+}
+</style>
---
+++ components/curriculum/curriculum-content.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="curriculum_content">
+    <div class="content_wrap">
+      <div class="video_area"></div>
+      <div class="video_title_area">
+        <p> 아이폰개발 시작하기 / 초보개발자 / 기본 튜토리얼 / 스위..</p>
+      </div>
+      <div class="video_like_area">
+        <div class="icon_content">
+          <font-awesome-icon icon="play"/>
+          <span>300</span>
+        </div>
+        <div class="icon_content">
+          <font-awesome-icon icon="heart"/>
+          <span>1521</span>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "curriculum-content"
+}
+</script>
+
+<style lang="scss" scoped>
+#curriculum_content {
+  margin-top: 30px;
+  height: 400px;
+  .content_wrap {
+    background: #FFFFFF 0 0 no-repeat padding-box;
+    border: 1px solid #DFDFDF;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    box-sizing: border-box;
+    width: 100%;
+
+    .video_area {
+      width: 100%;
+      height: 249px;
+      background: #484747;
+    }
+
+    .video_title_area {
+      padding: 30px 0 20px 20px;
+      font-size: 1.15rem;
+
+      p {
+        padding-bottom: 30px;
+        border-bottom: 1px solid #DFDFDF;
+      }
+    }
+
+    .video_like_area {
+      padding: 0 20px 20px 20px;
+      color: #B6B6B6;
+      display: flex;
+      justify-content: flex-start;
+      .icon_content{
+        margin-right: 15px;
+      }
+    }
+  }
+}
+</style>
