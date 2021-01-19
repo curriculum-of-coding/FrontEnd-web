17dd4c2d3639d98196256b93d40d6b0091d762fb
종훈 <leejonghun>
2021-01-19 19:59:28 +0900

    add side menu

---
+++ layouts/side-menu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="side"></div>
+</template>
+
+<script>
+export default {
+  name: "side-menu"
+}
+</script>
+
+<style lang="scss" scoped>
+.side {
+  width: 163px;
+  height: 100vh;
+  background: #354151;
+}
+</style>
