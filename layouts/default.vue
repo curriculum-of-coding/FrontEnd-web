01456a8214348376ad6e626f2dbee76bce50c337
종훈 <leejonghun>
2021-01-19 19:59:49 +0900

    set default vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,27 @@
 <template>
-<div>
-  테스팅
-</div>
+  <div class="main">
+    <side-menu/>
+    <Nuxt/>
+  </div>
 </template>
 
 <script>
+export {
+  defineComponent, onMounted, onBeforeMount, ref, reactive, toRefs, computed, watch
+} from '@vue/composition-api'
+import sideMenu from '@/layouts/side-menu'
+
 export default {
-  data () {
+  data() {
     return {}
-    }
+  },
+  components: {
+    sideMenu,
+  }
 }
 </script>
+<style lang="scss">
+.main{
+  display: flex;
+}
+</style>
