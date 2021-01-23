f01b2b4f0933b0c12a0e3b0cb8579f0dd09d9b00
wsj7178 <wsj7178@gmail.com>
2021-01-23 16:47:41 +0900

    add sidemenu #9

--- plugins/fontAwesome.ts
+++ plugins/fontAwesome.ts
@@ -1,9 +1,31 @@
 import Vue from 'vue'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faThumbsUp ,faThumbsDown,faChevronRight,faPlay,faHeart} from '@fortawesome/free-solid-svg-icons'
+import {
+  faThumbsUp,
+  faThumbsDown,
+  faChevronRight,
+  faChevronLeft,
+  faPlay,
+  faHeart,
+  faHome,
+  faBookOpen,
+  faChalkboard,
+} from '@fortawesome/free-solid-svg-icons'
+import { faThumbsUp as farThumbsUp } from '@fortawesome/free-regular-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-library.add(faThumbsUp,faThumbsDown,faChevronRight,faPlay,faHeart)
+library.add(
+  faThumbsUp,
+  faThumbsDown,
+  faChevronRight,
+  faChevronLeft,
+  faPlay,
+  faHeart,
+  faHome,
+  farThumbsUp,
+  faBookOpen,
+  faChalkboard,
+)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
---
+++ types/menu.ts
@@ -0,0 +1,9 @@
+type Menu = {
+  icon?: string | Array<string>,
+  name: string,
+  url: string,
+  submenu?: Menu[],
+  active?: Boolean,
+}
+
+export default Menu
