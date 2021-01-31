aa514f6adc6e2c1b6da25dadad7ec9c7dce3812f
종훈 <leejonghun>
2021-02-01 00:33:38 +0900

    reset signup

--- pages/user/signup.vue
+++ pages/user/signup.vue
@@ -1,191 +1,189 @@
 <template>
-  <div class="backBox">
-    <div class="inContents">
-
-      <header>
-        <div class="head_title">계정 정보를 입력해주세요.</div>
-        <div class="signOut" onclick="location.href='/user/login'">X</div>
-      </header>
-      <section>
-        <div class="sec_name">아이디 (이메일)</div>
-        <input type="text" class="sec_input" v-model="id" placeholder="예) email@email.co.kr">
-      </section>
-      <section>
-        <div class="sec_name">비밀번호</div>
-        <input type="text" class="sec_input" v-model="pw" placeholder="8자리 이상/영문,숫자,특수문자 사용">
-      </section>
-      <section>
-        <div class="sec_name">비밀번호 찾기 질문</div>
-        <select v-model="pwq_selected" class="sign_select">
-          <option disabled selected>질문을 선택해주세요.</option>
-          <option>1+1은?</option>
-          <option>처음으로 키운 강아지 이름은?</option>
-          <option>태어난 동네 이름은?</option>
-          <option>주민등록번호</option>
-        </select>
-      </section>
-      <section>
-        <div class="sec_name">비밀번호 찾기 답변</div>
-        <input type="text" class="sec_input" v-model="pwAnswer" placeholder="비밀번호 찾기 답변을 입력해주세요.">
-      </section>
-      <section>
-        <div class="sec_name">관심 분야</div>
-        <select v-model="secq_selected" class="sign_select">
-          <option disabled selected>관심분야를 선택해주세요.</option>
-          <option>Web</option>
-          <option>App</option>
-          <option>우주 산업</option>
-          <option>영상 / 그래픽</option>
-        </select>
-      </section>
-      <section>
-        <div class="sign_checkBox">
-          <v-checkbox class="sign_checkBox" label=" 모두동의">
-          </v-checkbox>
-        </div>
-        <div class="sign_checkBox">
-          <v-checkbox class="sign_checkBox" label=" (필수)서비스 이용약관">
-          </v-checkbox>
-          <a class="sign_detail">자세히 보기</a>
-        </div>
-        <div class="sign_checkBox">
-          <v-checkbox class="sign_checkBox" label=" (필수)개인정보 처리방침">
-          </v-checkbox>
-          <a class="sign_detail">자세히 보기</a>
-        </div>
-      </section>
-    </div>
-  </div>
+	<div class="signup">
+		<div class="header_area">
+			<h1 class="title">계정 정보를 입력해주세요</h1>
+			<font-awesome-icon icon="times" class="close_btn" />
+		</div>
+		<div class="input_area">
+			<customInput
+				:inputLabelID="'아이디(이메일)'"
+				:inputType="'email'"
+				:validation="emailValidationChecker.validation"
+				:validationContent="'올바른 이메일 주소를 입력해주세요.'"
+				@SetInputValue="getEmail"
+			></customInput>
+		</div>
+		<div class="input_area">
+			<customInput
+				:inputLabelID="'비밀번호'"
+				:inputType="'password'"
+				:placeholder="'8자리 이상 / 영문, 숫자, 특수문자 사용'"
+				:validation="passwordValidationChecker.validation"
+				:validationContent="'특수문자는 최소 1자 입력해주세요.'"
+				@SetInputValue="getPassword"
+			></customInput>
+		</div>
+		<customSelectBox
+			:selectLabelID="'비밀번호 찾기 질문'"
+			:select-options="passwordSelect"
+			v-model="formData.PWD_QUEST_TYPE"
+			@setSelectPW="getPWSelect"
+		></customSelectBox>
+		<div class="input_area">
+			<customInput
+				:inputLabelID="'비밀번호 찾기 답변'"
+				:inputType="'text'"
+				:placeholder="'비밀번호 찾기 답변을 입력해주세요.'"
+				:validation="passwordValidationCheckerAnswer.validation"
+				:validationContent="'답변을 3자 이상 입력해주세요'"
+				@SetInputValue="getPasswordAnswer"
+			></customInput>
+		</div>
+		<div class="input_area">
+			<customCheckBox
+				@setCheckbox="getCheckbox"
+				:checkBoxArray="checkBoxItems"
+			></customCheckBox>
+		</div>
+	</div>
 </template>
 
-<script>
+<script lang="ts">
+import customSelectBox from '@/components/common/common-select';
+import customInput from '@/components/common/common-input';
+import customCheckBox from '@/components/common/common-checkbox';
+import { reactive } from '@nuxtjs/composition-api';
+
+const emailRule = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+const passwordRule = /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,50}$/;
+
 export default {
-  data: function () {
-    return {
-      form:{
-        id: '',
-        pw: '',
-        pwAnswer: '',
-        secq_selected: '',
-        pwq_selected: ''
-      }
-    }
-  }
-}
+	components: {
+		customSelectBox,
+		customInput,
+		customCheckBox,
+	},
+	setup() {
+		const passwordSelect = [
+			{
+				PWD_QUEST_TYPE: 0,
+				options: '1+1은?',
+			},
+			{
+				PWD_QUEST_TYPE: 1,
+				options: '처음으로 키운 강아지 이름은?',
+			},
+			{
+				PWD_QUEST_TYPE: 2,
+				options: '태어난 동네 이름은?',
+			},
+		];
+		const checkBoxItems = [
+			{ id: '프론트앤드', check: false },
+			{ id: '백앤드', check: false },
+			{ id: '데브옵스', check: false },
+		];
+		const formData = {
+			USER_ID: '0',
+			EMAIL: 'abcd@naver.com',
+			USER_PWD: '1q2w3e4r5t!',
+			REG_DATE: '20210118',
+			PWD_QUEST_TYPE: '0',
+			PWD_ANSWER: '이게 나다 리발',
+			INTEREST_FRONT: 'Y',
+			INTEREST_BACK: 'Y',
+			INTEREST_DEVOPS: 'Y',
+			TOS_YN: 'Y',
+			PP_YN: 'Y',
+		};
+		const getEmail = value => {
+			formData.EMAIL = value;
+			emailValidationChecker.validation = emailRule.test(formData.EMAIL);
+		};
 
+		const getPassword = value => {
+			formData.USER_PWD = value;
+			console.log(formData.USER_PWD);
+			passwordValidationChecker.validation = passwordRule.test(
+				formData.USER_PWD,
+			);
+		};
+		const getPasswordAnswer = value => {
+			formData.PWD_ANSWER = value;
+			passwordValidationCheckerAnswer.validation =
+				formData.PWD_ANSWER.length >= 3;
+		};
+		let emailValidationChecker = reactive({
+			validation: true,
+		});
+		let passwordValidationChecker = reactive({
+			validation: true,
+		});
+		let passwordValidationCheckerAnswer = reactive({
+			validation: true,
+		});
+		const getPWSelect = value => {
+			return (formData.PWD_QUEST_TYPE = value);
+		};
+		const getCheckbox = value => {
+			interests(value);
+		};
+		const interests = value => {
+			formData.INTEREST_FRONT = !!value.includes('프론트앤드');
+			formData.INTEREST_BACK = !!value.includes('백앤드');
+			formData.INTEREST_DEVOPS = !!value.includes('데브옵스');
+		};
+		return {
+			getPWSelect,
+			checkBoxItems,
+			formData,
+			passwordSelect,
+			getEmail,
+			interests,
+			getPassword,
+			getPasswordAnswer,
+			emailValidationChecker,
+			passwordValidationChecker,
+			passwordValidationCheckerAnswer,
+			getCheckbox,
+		};
+	},
+};
 </script>
-
 <style>
 body {
-  background-color: #354151;
+	background: #354151;
 }
+</style>
+<style lang="scss" scoped>
+.signup {
+	padding: 34px 40px 60px 40px;
+	width: 440px;
+	height: 857px;
+	background: #fff;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
 
-.backBox {
-  height: fit-content;
-  width: fit-content;
-  padding: 2rem;
-  margin: auto;
-  margin-top: 10%;
-  border-radius: 0.9rem;
-  background-color: white;
+	.header_area {
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 55px;
+
+		.close_btn {
+			font-size: 30px;
+			font-weight: normal;
+			color: #b3b3b3;
+		}
+
+		.title {
+			text-align: left;
+			font: normal normal bold 26px/35px NanumSquareRound;
+			letter-spacing: -1.3px;
+			color: #0d0d0d;
+			opacity: 1;
+		}
+	}
 }
-
-.inContents {
-  text-align: center;
-  height: fit-content;
-  width: max-content;
-  margin: auto;
-}
-
-header {
-  position: relative;
-  margin-bottom: 2.2rem;
-}
-
-.head_title {
-  font-size: 17pt;
-  margin-left: -4rem;
-}
-
-section {
-  margin-bottom: 1.2rem;
-}
-
-
-option {
-  font-weight: bold;
-  font-size: 11pt;
-}
-
-.signOut {
-  position: absolute;
-  text-align: right;
-  width: 2rem;
-  right: 0px;
-  margin-top: -2.8rem;
-  margin-right: -0.5rem;
-  font-size: 22pt;
-  font-weight: normal;
-  color: #B6B6B6;
-}
-
-.signOut:hover {
-  font-weight: bolder;
-  color: #47494e;
-}
-
-.sec_name {
-  text-align: left;
-  font-size: 12pt;
-  font-weight: bold;
-  margin-bottom: 0.3rem;
-  color: #354151;
-}
-
-.sec_input {
-  border: #969696 thin solid;
-  border-radius: 2rem;
-  font-size: 10pt;
-  padding: 1rem;
-  height: 2.1rem;
-  width: 20rem;
-}
-
-.sign_select {
-  background: url("https://cdn.iconscout.com/icon/free/png-256/down-arrow-16-460295.png") no-repeat 95% 50%;
-  background-size: 2rem;
-  color: #354151;
-  border: #969696 thin solid;
-  border-radius: 2rem;
-  font-size: 10pt;
-  height: 2.1rem;
-  width: 20rem;
-  padding-left: 1rem;
-}
-
-.sec_input::placeholder {
-  color: #B6B6B6;
-}
-
-.sign_checkBox {
-  position: relative;
-  margin: 0px;
-  margin-bottom: 0.7rem;
-  height: 1.5rem;
-}
-
-.sign_detail {
-  position: absolute;
-  font-weight: bold;
-  font-size: 11pt;
-  text-decoration: underline;
-  color: #969696;
-  bottom: -0.2rem;
-  right: 0px;
-}
-
-.theme--light.v-icon {
-  color: #F64E5B;
-}
-
 </style>
