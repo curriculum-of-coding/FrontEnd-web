e429f9a43d020cbed538c8030497f111fb4829a7
dongyub <9754745@naver.com>
2021-01-31 20:33:51 +0900

    뷰 적용

--- pages/user/signup.vue
+++ pages/user/signup.vue
@@ -3,64 +3,76 @@
     <div class="inContents">
 
       <header>
-        <div class="head_title">계정 정보를 입력해주세요.</div><div class="signOut" onclick="location.href='/user/login'">X</div>
+        <div class="head_title">계정 정보를 입력해주세요.</div>
+        <div class="signOut" onclick="location.href='/user/login'">X</div>
       </header>
-      <form>
-        <section>
-          <div class="sec_name">아이디 (이메일)</div>
-          <input type="text" class="sec_input" name="id" placeholder="예) email@email.co.kr">
-        </section>
-        <section>
-          <div class="sec_name">비밀번호</div>
-          <input type="text" class="sec_input" name="pw" placeholder="8자리 이상/영문,숫자,특수문자 사용">
-        </section>
-        <section>
-          <div class="sec_name">비밀번호 찾기 질문</div>
-          <select type="text" class="sign_select" name="pwQuestion" title="1">
-            <option selected>질문을 선택해주세요.</option>
-            <option>1+1은?</option>
-            <option>처음으로 키운 강아지 이름은?</option>
-            <option>태어난 동네 이름은?</option>
-            <option>주민등록번호</option>
-          </select>
-        </section>
-        <section>
-          <div class="sec_name">비밀번호 찾기 답변</div>
-          <input type="text" class="sec_input" name="pwAnswer" placeholder="비밀번호 찾기 답변을 입력해주세요.">
-        </section>
-        <section>
-          <div class="sec_name">관심 분야</div>
-          <select type="text" class="sign_select" name="subject">
-            <option selected>전부다</option>
-            <option>Web</option>
-            <option>App</option>
-            <option>우주 산업</option>
-            <option>영상 / 그래픽</option>
-          </select>
-        </section>
-        <section>
-          <div class="sign_checkBox">
-            <v-checkbox class="sign_checkBox" label=" 모두동의">
-            </v-checkbox>
-          </div>
-          <div class="sign_checkBox">
-            <v-checkbox class="sign_checkBox" label=" (필수)서비스 이용약관">
-            </v-checkbox>
-            <a class="sign_detail">자세히 보기</a>
-          </div>
-          <div class="sign_checkBox">
-            <v-checkbox class="sign_checkBox" label=" (필수)개인정보 처리방침">
-            </v-checkbox>
-            <a class="sign_detail">자세히 보기</a>
-          </div>
-        </section>
-      </form>
+      <section>
+        <div class="sec_name">아이디 (이메일)</div>
+        <input type="text" class="sec_input" v-model="id" placeholder="예) email@email.co.kr">
+      </section>
+      <section>
+        <div class="sec_name">비밀번호</div>
+        <input type="text" class="sec_input" v-model="pw" placeholder="8자리 이상/영문,숫자,특수문자 사용">
+      </section>
+      <section>
+        <div class="sec_name">비밀번호 찾기 질문</div>
+        <select v-model="pwq_selected" class="sign_select">
+          <option disabled selected>질문을 선택해주세요.</option>
+          <option>1+1은?</option>
+          <option>처음으로 키운 강아지 이름은?</option>
+          <option>태어난 동네 이름은?</option>
+          <option>주민등록번호</option>
+        </select>
+      </section>
+      <section>
+        <div class="sec_name">비밀번호 찾기 답변</div>
+        <input type="text" class="sec_input" v-model="pwAnswer" placeholder="비밀번호 찾기 답변을 입력해주세요.">
+      </section>
+      <section>
+        <div class="sec_name">관심 분야</div>
+        <select v-model="secq_selected" class="sign_select">
+          <option disabled selected>관심분야를 선택해주세요.</option>
+          <option>Web</option>
+          <option>App</option>
+          <option>우주 산업</option>
+          <option>영상 / 그래픽</option>
+        </select>
+      </section>
+      <section>
+        <div class="sign_checkBox">
+          <v-checkbox class="sign_checkBox" label=" 모두동의">
+          </v-checkbox>
+        </div>
+        <div class="sign_checkBox">
+          <v-checkbox class="sign_checkBox" label=" (필수)서비스 이용약관">
+          </v-checkbox>
+          <a class="sign_detail">자세히 보기</a>
+        </div>
+        <div class="sign_checkBox">
+          <v-checkbox class="sign_checkBox" label=" (필수)개인정보 처리방침">
+          </v-checkbox>
+          <a class="sign_detail">자세히 보기</a>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: function () {
+    return {
+      form:{
+        id: '',
+        pw: '',
+        pwAnswer: '',
+        secq_selected: '',
+        pwq_selected: ''
+      }
+    }
+  }
+}
+
 </script>
 
 <style>
@@ -98,7 +110,6 @@ header {
 section {
   margin-bottom: 1.2rem;
 }
-
 
 
 option {
