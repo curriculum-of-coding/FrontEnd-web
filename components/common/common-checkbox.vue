7e1342fea40ac3b7a0057c196175b8879d2f8aae
종훈 <leejonghun>
2021-02-01 00:33:49 +0900

    add common component

---
+++ components/common/common-checkbox.vue
@@ -0,0 +1,77 @@
+<template>
+	<div>
+		<div class="checkboxTitle">관심분야</div>
+		<div class="common_checkbox_wrap">
+			<div
+				v-for="(items, index) in checkBoxArray"
+				:key="index"
+				class="common_checkbox_content"
+			>
+				<label :for="items.id">
+					{{ items.id }}
+					<input
+						type="checkbox"
+						v-model="checkBoxPicker"
+						:id="items.id"
+						:value="items.id"
+						@change="setCheckbox(checkBoxPicker)"
+					/>
+				</label>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { reactive } from '@nuxtjs/composition-api';
+
+export default {
+	name: 'common-checkbox',
+	props: {
+		checkBoxArray: {
+			type: Array,
+		},
+
+		checkBoxId: {
+			type: Object,
+		},
+	},
+	setup(props, { emit }) {
+		const checkBoxPicker = reactive([]);
+		const setCheckbox = value => {
+			return emit('setCheckbox', value);
+		};
+		return { checkBoxPicker, setCheckbox };
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.checkboxTitle {
+	text-align: left;
+	font: normal normal normal 18px/24px NanumSquareRound;
+	letter-spacing: -0.9px;
+	color: #0d0d0d;
+}
+.common_checkbox_wrap {
+	display: flex;
+	justify-content: flex-start;
+	.common_checkbox_content {
+		margin-top: 10px;
+		label {
+			cursor: pointer;
+			font: normal normal normal 18px/24px NanumSquareRound;
+			letter-spacing: -0.9px;
+			color: #0d0d0d;
+			input {
+				cursor: pointer;
+			}
+		}
+		&:first-child {
+			margin-left: 0;
+		}
+
+		margin-left: 10px;
+	}
+}
+</style>
