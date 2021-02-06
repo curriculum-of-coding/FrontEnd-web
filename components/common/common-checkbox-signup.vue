a57dafed19ecd78aa1694350e555c3eb924b643e
종훈 <leejonghun>
2021-02-06 23:46:26 +0900

    change contents

--- components/common/common-checkbox-signup.vue
+++ components/common/common-checkbox-signup.vue
@@ -1,181 +1,214 @@
 <template>
-	<div>
-		<div class="checkboxTitle">{{ headerTitle }}</div>
-		<div class="common_checkbox_content" :class="[column ? 'column' : '']">
-			<label class="checkbox" style="--color: #FE3C53;">
-				<span class="checkbox_input">
-					<input type="checkbox" />
-					<span class="checkbox_control">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							aria-hidden="true"
-							focusable="false"
-						>
-							<path
-								fill="none"
-								stroke="currentColor"
-								stroke-width="3"
-								d="M1.73 12.91l6.37 6.37L22.79 4.59"
-							/>
-						</svg>
-					</span>
-				</span>
-				<span class="checkbox_value">
-					모두 동의
-				</span>
-			</label>
-			<div v-for="(items, index) in checkBoxArray" :key="index">
-				<label class="checkbox" style="--color: #FE3C53;">
-					<span class="checkbox_input">
-						<input
-							type="checkbox"
-							v-model="checkBoxPicker"
-							:id="items.id"
-							:value="items.id"
-							@change="setCheckbox(checkBoxPicker)"
-						/>
-						<span class="checkbox_control">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								aria-hidden="true"
-								focusable="false"
-							>
-								<path
-									fill="none"
-									stroke="currentColor"
-									stroke-width="3"
-									d="M1.73 12.91l6.37 6.37L22.79 4.59"
-								/>
-							</svg>
-						</span>
-					</span>
-					<span class="checkbox_value">
-						<span>{{ items.id }}</span>
-						<span @click="openModal">{{ items.contentType }}</span>
-					</span>
-				</label>
-			</div>
-		</div>
-	</div>
+  <div>
+    <div class="checkboxTitle">{{ headerTitle }}</div>
+    <div class="common_checkbox_content" :class="[column ? 'column' : '']">
+      <label class="checkbox" style="--color: #FE3C53;">
+        <span class="checkbox_input">
+          <input type="checkbox" />
+          <span class="checkbox_control">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+              focusable="false"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                stroke-width="3"
+                d="M1.73 12.91l6.37 6.37L22.79 4.59"
+              />
+            </svg>
+          </span>
+        </span>
+        <span class="checkbox_value">
+          모두 동의
+        </span>
+      </label>
+      <div
+        v-for="(items, index) in checkBoxArray"
+        :key="index"
+        class="checkbox_flex"
+      >
+        <label class="checkbox" style="--color: #FE3C53;">
+          <span class="checkbox_input">
+            <input
+              type="checkbox"
+              v-model="checkBoxPicker"
+              :id="items.id"
+              :value="items.id"
+              @change="setCheckbox(checkBoxPicker)"
+            />
+            <span class="checkbox_control">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+                focusable="false"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="3"
+                  d="M1.73 12.91l6.37 6.37L22.79 4.59"
+                />
+              </svg>
+            </span>
+          </span>
+          <span class="checkbox_value">
+            <span>{{ items.id }}</span>
+          </span>
+        </label>
+        <span @click="openModal(items.contentType)">
+          {{ items.contentType }}</span
+        >
+      </div>
+      <commonModal
+        @setModal="getModalStatus"
+        :setModal="openModalCheck.value"
+        :modal-header="modalHeader.header"
+      ></commonModal>
+    </div>
+  </div>
 </template>
 
 <script>
+import commonModal from '@/components/common/common-modal';
 import { reactive } from '@nuxtjs/composition-api';
-import commonModal from '~/components/common/common-modal';
 
 export default {
-	name: 'common-checkbox-signup',
-	components: {
-		commonModal,
-	},
-	props: {
-		checkBoxArray: {
-			type: Array,
-		},
+  name: 'common-checkbox-signup',
+  components: {
+    commonModal,
+  },
+  props: {
+    checkBoxArray: {
+      type: Array,
+    },
 
-		checkBoxId: {
-			type: Object,
-		},
-		column: {
-			type: Boolean,
-			default: false,
-		},
-		headerTitle: {
-			type: String,
-			default: '',
-		},
-	},
-	setup(props, { emit }) {
-		let openModalCheck = true;
-		const checkBoxPicker = reactive([]);
-		const setCheckbox = value => {
-			return emit('setCheckbox', value);
-		};
-		const openModal = () => {
-			openModalCheck = true;
-		};
-		return { checkBoxPicker, setCheckbox, openModal, openModalCheck };
-	},
+    checkBoxId: {
+      type: Object,
+    },
+    column: {
+      type: Boolean,
+      default: false,
+    },
+    headerTitle: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props, { emit }) {
+    let openModalCheck = reactive({ value: null });
+    let modalHeader = reactive({ header: '' });
+    const checkBoxPicker = reactive([]);
+
+    const setCheckbox = value => {
+      return emit('setCheckbox', value);
+    };
+    const openModal = value => {
+      modalHeader.header = value;
+      openModalCheck.value = !openModalCheck.value;
+    };
+    const getModalStatus = value => {
+      openModalCheck.value = value;
+    };
+    return {
+      checkBoxPicker,
+      setCheckbox,
+      openModal,
+      openModalCheck,
+      getModalStatus,
+      modalHeader,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .checkboxTitle {
-	text-align: left;
-	font: normal normal normal 18px/24px NanumSquareRound;
-	letter-spacing: -0.9px;
-	color: #0d0d0d;
+  text-align: left;
+  font: normal normal normal 18px/24px NanumSquareRound;
+  letter-spacing: -0.9px;
+  color: #0d0d0d;
 }
+
 .common_checkbox_content {
-	margin-top: 10px;
-	display: flex;
-	justify-content: flex-start;
-	&.column {
-		flex-direction: column;
-	}
-	.checkbox {
-		grid-template-columns: min-content auto;
-		grid-gap: 0.5em;
-		display: flex;
-		align-items: center;
-		color: #fff;
-		margin-bottom: 16px;
-		margin-right: 10px;
+  margin-top: 10px;
+  display: flex;
+  justify-content: flex-start;
 
-		&--disabled {
-			background: none;
-			color: #fff;
-		}
-	}
+  &.column {
+    flex-direction: column;
+  }
 
-	.checkbox_control {
-		display: inline-grid;
-		width: 1em;
-		height: 1em;
-		border-radius: 0.25em;
-		border: 1px solid var(--color);
+  .checkbox {
+    grid-template-columns: min-content auto;
+    grid-gap: 0.5em;
+    display: flex;
+    align-items: center;
+    color: #fff;
+    margin-bottom: 16px;
+    margin-right: 10px;
 
-		svg {
-			transition: transform 0.1s ease-in 25ms;
-			transform: scale(0);
-			transform-origin: bottom left;
-		}
-	}
+    &--disabled {
+      background: none;
+      color: #fff;
+    }
+  }
 
-	.checkbox_input {
-		display: grid;
-		grid-template-areas: 'checkbox';
+  .checkbox_control {
+    display: inline-grid;
+    width: 1em;
+    height: 1em;
+    border-radius: 0.25em;
+    border: 1px solid var(--color);
 
-		> * {
-			grid-area: checkbox;
-		}
+    svg {
+      transition: transform 0.1s ease-in 25ms;
+      transform: scale(0);
+      transform-origin: bottom left;
+    }
+  }
 
-		input {
-			opacity: 0;
-			width: 1em;
-			height: 1em;
+  .checkbox_input {
+    display: grid;
+    grid-template-areas: 'checkbox';
 
-			&:focus + .checkbox_control {
-				box-shadow: 0 0 0 0.05em #fff, 0 0 0.15em 0.1em currentColor;
-			}
+    > * {
+      grid-area: checkbox;
+    }
 
-			&:checked + .checkbox_control svg {
-				transform: scale(1);
-			}
+    input {
+      opacity: 0;
+      width: 1em;
+      height: 1em;
 
-			&:checked + .checkbox_control {
-				background: var(--color);
-			}
-		}
-	}
+      &:focus + .checkbox_control {
+        box-shadow: 0 0 0 0.05em #fff, 0 0 0.15em 0.1em currentColor;
+      }
+
+      &:checked + .checkbox_control svg {
+        transform: scale(1);
+      }
+
+      &:checked + .checkbox_control {
+        background: var(--color);
+      }
+    }
+  }
 }
+
 .checkbox_value {
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	width: 100%;
-	color: #000 !important;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  color: #000 !important;
+}
+
+.checkbox_flex {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
--- components/common/common-modal.vue
+++ components/common/common-modal.vue
@@ -1,16 +1,50 @@
 <template>
-	<div></div>
+  <v-row justify="center">
+    <v-dialog v-model="setModal" persistent width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ modalHeader }}</span>
+        </v-card-title>
+        <v-card-text v-if="modalHeader === 'tos'">
+          tos 유저
+        </v-card-text>
+        <v-card-text v-else>
+          pp 유저
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="close">
+            Disagree
+          </v-btn>
+          <v-btn color="green darken-1" text @click="close">
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
 </template>
 
 <script>
+import { reactive } from '@nuxtjs/composition-api';
+
 export default {
-	name: 'common-modal',
-	props: {
-		dialog: {
-			type: Boolean,
-			default: true,
-		},
-	},
+  name: 'common-modal',
+  props: {
+    setModal: {
+      default: false,
+    },
+    modalHeader: {
+      default: '',
+    },
+  },
+  setup(props, { emit }) {
+    let openModalCheck = reactive(props.setModal);
+    const close = () => {
+      return emit('setModal', openModalCheck);
+    };
+    return { close, openModalCheck };
+  },
 };
 </script>
 
