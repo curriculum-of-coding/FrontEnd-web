ce55419355a0fcc16476d6bb31fd8f6e3b916998
종훈 <leejonghun>
2021-02-10 16:27:07 +0900

    change logic

--- pages/index.vue
+++ pages/index.vue
@@ -8,15 +8,17 @@
 <script lang="ts">
 import tabMenu from '@/components/tab-menu.vue';
 import curriculumWrap from '~/components/curriculum/curriculum-content-wrap.vue';
+import { reactive } from '@nuxtjs/composition-api';
 
 export default {
   setup() {
-    const tabItem = [
+    const tabItem = reactive([
       { name: '분야별 TOP1', field: 'TOP', active: false },
       { name: 'Front-end', field: 'FE', active: true },
       { name: 'Back-end', field: 'BE', active: false },
       { name: 'Dev-ops', field: 'DO', active: false },
-    ];
+    ]);
+
     return { tabItem };
   },
 
