e826b1064f1b24eb5a918224c6f82eae35c0feb1
종훈 <leejonghun>
2021-02-20 18:25:01 +0900

    add menutitle store

---
+++ store/menuTitle.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+  menuTitle: {
+    name: '커리큘럼',
+    subTitle: '이런 강의는 어떠신가요? 현직자들이 추천하는 커리큘럼입니다.',
+  },
+});
+
+export const mutations = {
+  setMenuTitle(state, menuTitle) {
+    state.menuTitle = menuTitle;
+  },
+};
+
+export const actions = {
+  setMenuTitle: ({ commit }, menuTitle) => {
+    commit('setMenuTitle', menuTitle);
+  },
+};
+export const strict = false;
